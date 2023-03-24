d014ce32d67167e093b67196a6a645c2ee5517d3
markkoos <markusimercado@gmail.com>
2023-03-23 17:03:29 -0700

    added logic for navbar; need to style

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <center>
         <h1>Search for Players</h1>
-        {/* Search Bar to look up NBA Players */}
+        {/* Search Bar to look up Players */}
         <div class="input-group input-group-lg">
             <input type="text" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm"></input>
         </div>
