9f039f8f74f53f9dfe64332d7128ff93ae5d8f12
markkoos <markusimercado@gmail.com>
2023-03-27 13:39:40 -0700

    renders entire container to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/pages/Home';
-import NavBar from './components/NavBar';
+import Container from './components/Container';
 
 function App() {
   return (
-    <NavBar />
+    <Container />
   );
 }
 
