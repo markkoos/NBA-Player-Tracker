d1fe033c9be0a0993ddf351b2f1a1d99da08f559
markkoos <markusimercado@gmail.com>
2023-04-03 14:48:39 -0700

    added login and register pages and reworked container and routes to work properly

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // importing bootstrap so it can be used in application
 import 'bootstrap/dist/css/bootstrap.css';
@@ -12,12 +13,10 @@ import style from "./styles/style.css";
 
 console.log(`using ${style}`);
 
-// rendering the app component to the DOM
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// ReactDOM.render() method to renders our app component to the DOM
+ReactDOM.render(<App />,
+  // Rendering app component to the 'root' element which is the default ID created with render method
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
