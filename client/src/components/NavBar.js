e0dd68894da4848408bccb878fad130e758e2595
markkoos <markusimercado@gmail.com>
2023-04-04 16:24:44 -0700

    added logo to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import Logo from '../assets/bball.png';
 // hi
 
 export default function NavBar({ pageHandler }) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <span className="navbar-brand">NBA Player Tracker</span>
+            <span className="navbar-brand"><img className="logo" src={Logo}></img>NBA PLAYER TRACKER</span>
             <ul className="navbar-nav mr-auto">
 
                 <li className="nav-item">
-                <a href="home" onClick={() => pageHandler('Home')}>Home</a>
+                <a href="home" onClick={() => pageHandler('Home')}>HOME</a>
                 </li>
 
                 <li className="nav-item">
-                <a href="profile" onClick={() => pageHandler('Profile')}>Profile</a>
+                <a href="profile" onClick={() => pageHandler('Profile')}>PROFILE</a>
                 </li>
 
                 <li className="nav-item">
-                <a href="login" onClick={() => pageHandler('Login')}>Login</a>
+                <a href="login" onClick={() => pageHandler('Login')}>LOGIN</a>
                 </li>
 
             </ul>
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 export default function Home() {
     return (
-        <center>
-        <h1>Search for Players</h1>
-        {/* Search Bar to look up Players */}
-        <div className="input-group input-group-lg">
-            <input type="text" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm"></input>
+        <div>
+            <br></br>
+            <center>
+            <h1>Search for Players</h1>
+            {/* Search Bar to look up Players */}
+            <div className="input-group input-group-lg">
+                <input type="text" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm"></input>
+            </div>
+            </center>
         </div>
-        </center>
     )
 }
