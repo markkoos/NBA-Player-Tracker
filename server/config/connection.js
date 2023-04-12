1bccc2ac81b36340fb98b927a3eb436f2384678b
markkoos <markusimercado@gmail.com>
2023-04-11 22:27:56 -0700

    added .env code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // mongodb://127.0.0.1:27017 (add database name)
-mongoose.connect('mongodb+srv://markkoos:Grill12g!@cluster0.ltis3af.mongodb.net/nbaplayertracker?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ltis3af.mongodb.net/${process.env.USER_DB}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
